3cc3a2f3099f06a4ce28a66e413e6a55125a62f5
panicmarvin <516831067@qq.com>
2023-04-17 11:54:25 -0400

    add assertion

---
+++ excercises/test0/apb_slave.f
@@ -0,0 +1,22 @@
++define+ASSERT_ON
++define+ARM_ASSERT_ON
++define+OVL_ASSERT_ON
++define+ARM_APB_ASSERT_ON
+//+define+ARM_AHB_ASSERT_ON
+
++incdir+../../std_ovl
++libext+.vlib
+-y ../../std_ovl
+
+../../logical/models/clkgate/cmsdk_clock_gate.v
+../../excercises/logical/testbench/tb_frbm_example.v
+../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_fileread_master32.v
+../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_fileread_funnel.v
+../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_filereadcore.v
+../../logical/cmsdk_ahb_to_apb/verilog/cmsdk_ahb_to_apb.v
+
+../../excercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave.v
+../../excercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave_interface.v
+../../excercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave_reg.v
+
+
--- exercises/test0/apb_slave.f
+++
@@ -1,9 +0,0 @@
-../../../logical/cmsdk_ahb_fileread_masters/example/tb_frbm_example.v
-../../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_fileread_master32.v
-../../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_fileread_funnel.v
-../../../logical/cmsdk_ahb_fileread_masters/verilog/cmsdk_ahb_filereadcore.v
-../../../ogical/cmsdk_ahb_to_apb/verilog/cmsdk_ahb_to_apb.v
-
-../../../exercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave.v
-../../../exercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave_interface.v
-../../../exercises/logical/cmsdk_apb4_eg_slave/verilog/cmsdk_apb4_eg_slave_reg.v
